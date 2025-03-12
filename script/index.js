498d787eed05d91caec759333b281be4d34cad9e
Md Niaz Al Motin <niaz.al.motin@gmail.com>
2025-03-12 04:08:56 -0700

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,33 @@
+const names = ["Professor ", "Dramatist ", "Writer ", "Actor "];
+let nameIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+const typingSpeed = 100;
+const erasingSpeed = 50;
+const delayBetweenWords = 2000;
+const proNamesElement = document.getElementById("proNames");
+
+function typeEffect() {
+    if (!proNamesElement) return;
+    const currentName = names[nameIndex];
+    
+    if (isDeleting) {
+        proNamesElement.textContent = currentName.substring(0, charIndex--);
+    } else {
+        proNamesElement.textContent = currentName.substring(0, charIndex++);
+    }
+    
+    let speed = isDeleting ? erasingSpeed : typingSpeed;
+    
+    if (!isDeleting && charIndex === currentName.length) {
+        isDeleting = true;
+        speed = delayBetweenWords;
+    } else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        nameIndex = (nameIndex + 1) % names.length;
+    }
+    
+    setTimeout(typeEffect, speed);
+}
+
+typeEffect();
